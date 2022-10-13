c2589878c9f752a2503a3c4a5e0fcb94e0767308
Oleg Baranov <xssnick8@gmail.com>
2022-10-13 14:49:03 +0300

    Send mode changed to 1

--- build/registry.deploy.ts
+++ build/registry.deploy.ts
@@ -1,6 +1,16 @@
-import { Address, toNano, TupleSlice, WalletContract } from "ton";
-import {buildRegistryDataCell, Verifier} from "../packages/contracts/registry-contract/RegistryData";
+import {
+    Address,
+    CellMessage,
+    CommonMessageInfo,
+    InternalMessage,
+    SendMode,
+    toNano,
+    TupleSlice,
+    WalletContract
+} from "ton";
+import {buildRegistryDataCell, Queries, Verifier} from "../packages/contracts/registry-contract/RegistryData";
 import BN from "bn.js";
+import {randomKeyPair} from "../packages/utils/randomKeyPair";
 
 // return the init Cell of the contract storage (according to load_data() contract method)
 export function initData() {
@@ -19,4 +29,38 @@ export async function postDeployTest(walletContract: WalletContract, secretKey: 
     const call = await walletContract.client.callGetMethod(contractAddress, "get_verifiers_num");
     let ts = new TupleSlice(call.stack);
     console.log(`   # Getter 'get_verifiers_num' = ${ts.readNumber()}`);
+/*
+    let kp = await randomKeyPair()
+    let kp2 = await randomKeyPair()
+    let kp3 = await randomKeyPair()
+
+    const seqno = await walletContract.getSeqNo();
+    const transfer = walletContract.createTransfer({
+        secretKey: secretKey,
+        seqno: seqno,
+        sendMode: SendMode.PAY_GAS_SEPARATLY + SendMode.IGNORE_ERRORS,
+        order: new InternalMessage({
+            to: contractAddress,
+            value: toNano(1),
+            bounce: false,
+            body: new CommonMessageInfo({
+              //  body: new CellMessage(Queries.removeVerifier({
+               //     id: new BN(717),
+             //   }))
+                body: new CellMessage(Queries.updateVerifier({
+                    id: new BN(77187),
+                    quorum: 2,
+                    endpoints: new Map<BN, number>([
+                        [new BN(kp.publicKey), 1],
+                        [new BN(kp2.publicKey), 2],
+                        [new BN(kp3.publicKey), 3]
+                    ])
+                }))
+            }),
+        }),
+    });
+    await walletContract.client.sendExternalMessage(walletContract, transfer);
+
+    console.log(`   # Message sent`);
+ */
 }
